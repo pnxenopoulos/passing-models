a751eb3dad82057e9c269520fcab4c7eb35f0e6b
pnxenopoulos <pnxenopoulos@gmail.com>
2019-04-10 14:19:18 -0400

    started to link related passing pressure and receipt events

--- R/passing_model.R
+++ R/passing_model.R
@@ -9,7 +9,7 @@ world_cup_events <- statsbomb_data %>% filter(competition_id == 43)
 wsl_events <- statsbomb_data %>% filter(competition_id == 37)
 nwsl <- statsbomb_data %>% filter(competition_id == 49)
 
-test <- wsl_events %>% 
+passes <- wsl_events %>% 
     filter(type.name == "Pass") %>%
     select(id, 
            related_events,
@@ -41,5 +41,40 @@ test <- wsl_events %>%
            pass.outswinging,
            pass.deflected,
            pass.miscommunication,
-           pass.no_touch)
+           pass.no_touch) %>%
+    separate(location, into = c("z", "pass.start.x", "pass.start.y")) %>%
+    separate(pass.end_location, into = c("y", "pass.end.x", "pass.end.y")) %>%
+    select(-z, -y)
+
+receptions <- wsl_events %>% 
+    filter(type.name == "Ball Receipt*") %>%
+    select(related_events,
+           under_pressure)
+
+names(receptions) <- c("related_events", "receiver_pressured")
+
+passes$receiver_under_pressure <- FALSE
+passes$received <- FALSE
+for(i in nrow(passes)) {
+    rel_events <- unlist(passes$related_events[i])
+    for(event in rel_events) {
+        event_info <- wsl_events %>% 
+            filter(id == event) %>%
+            select(type.name, under_pressure)
+        under_press <- event_info$under_pressure
+        if(event_info$type.name == 'Ball Receipt*') {
+            passes$received[i] <- TRUE
+        } else if(event_info$type.name == "Dispossessed") {
+            passes$received[i] <- FALSE
+        } else if(event_info$type.name == "Ball Recovery") {
+            passes$received[i] <- FALSE
+        } else if(event_info$type.name == "Interception") {
+            passes$received[i] <- FALSE
+        } else if(event_info$type.name == "Block") {
+            passes$received[i] <- FALSE
+        } else {
+            passes$received[i] <- FALSE
+        }
+    }
     
+}
